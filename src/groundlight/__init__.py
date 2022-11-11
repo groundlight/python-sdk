c14c6d5d8d4ae4ef25d017a42dc043eff1f42046
Leo Dirac <leo@positronix.ai>
2022-11-10 17:42:52 -0800

    Setting groundlight.__version__

--- src/groundlight/__init__.py
+++ src/groundlight/__init__.py
@@ -1,6 +1,12 @@
+import importlib.metadata
+
 # flake8: noqa
 # Add useful imports from the generated code here at the top level, as a convenience.
 from openapi_client import ApiException
 
 # Imports from our code
 from .client import Groundlight
+
+# Copy the version number from where it's set in pyproject.toml
+__version__ = importlib.metadata.version("groundlight")
+
