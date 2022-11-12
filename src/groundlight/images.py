4fe4ef4086e615dc84a209a37b5188e95dd82de5
Leo Dirac <leo@positronix.ai>
2022-11-12 12:39:37 -0800

    fixing commented out error.

--- src/groundlight/images.py
+++ src/groundlight/images.py
@@ -21,5 +21,5 @@ def jpeg_from_numpy(img: np.ndarray, jpeg_quality: int = 95) -> io.BytesIO:
     pilim = Image.fromarray(img.astype("uint8"), "RGB")
     with io.BytesIO() as buf:
         pilim.save(buf, "jpeg", quality=jpeg_quality)
-        # out = buf.getvalue()
+        out = buf.getvalue()
         return out
