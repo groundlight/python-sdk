a16d2dacbeec9922152ce2f8c0524c9e3facfa03
Leo Dirac <leo@positronix.ai>
2022-11-12 10:13:30 -0800

    Adding code samples.

---
+++ samples/blocking_submit.py
@@ -0,0 +1,14 @@
+"""Example of how to wait for a confident result
+"""
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+from groundlight import Groundlight
+
+gl = Groundlight()
+
+d = gl.get_or_create_detector(name="dog", query="is there a dog in the picture?")
+
+print(f"Submitting image query")
+iq = gl.submit_image_query(d, image="../test/assets/dog.jpeg", wait=30)
+print(iq)
