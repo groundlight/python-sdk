a5bc89fcdf2d49f55373c631a7d2ffa1403f3b88
Leo Dirac <leo@positronix.ai>
2022-11-12 10:37:55 -0800

    Fixing py3.7 - not supporting version number, but not erroring

--- src/groundlight/__init__.py
+++ src/groundlight/__init__.py
@@ -1,5 +1,3 @@
-import importlib.metadata
-
 # flake8: noqa
 # Add useful imports from the generated code here at the top level, as a convenience.
 from openapi_client import ApiException
@@ -7,5 +5,11 @@ from openapi_client import ApiException
 # Imports from our code
 from .client import Groundlight
 
-# Copy the version number from where it's set in pyproject.toml
-__version__ = importlib.metadata.version("groundlight")
+try:
+    import importlib.metadata
+    # Copy the version number from where it's set in pyproject.toml
+    __version__ = importlib.metadata.version("groundlight")
+except ModuleNotFoundError:
+    # importlib.metadata was only added in py3.8
+    # We're still supporting py3.7
+    __version__ = "(version number available in python 3.8+)"
