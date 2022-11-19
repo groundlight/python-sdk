31e9bfc827f7ae71415484d6836c316908e7e5c6
Leo Dirac <leo@positronix.ai>
2022-11-19 12:18:08 -0800

    changing endpoint config

--- src/groundlight/config.py
+++ src/groundlight/config.py
@@ -3,10 +3,10 @@ import os
 API_TOKEN_WEB_URL = "https://app.groundlight.ai/reef/my-account/api-tokens"
 API_TOKEN_VARIABLE_NAME = "GROUNDLIGHT_API_TOKEN"
 
-GROUNDLIGHT_ENDPOINT = os.environ.get("GROUNDLIGHT_ENDPOINT", "https://api.groundlight.ai/device-api")
+DEFAULT_ENDPOINT = os.environ.get("GROUNDLIGHT_ENDPOINT", "https://api.groundlight.ai/device-api")
 
 __all__ = [
     "API_TOKEN_WEB_URL",
     "API_TOKEN_VARIABLE_NAME",
-    "GROUNDLIGHT_ENDPOINT",
+    "DEFAULT_ENDPOINT",
 ]
