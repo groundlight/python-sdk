65eb9c6c7485f932c2767a74898573b0dc85e380
Michael Vogelsong <4020875+mjvogelsong@users.noreply.github.com>
2023-04-05 14:38:41 -0700

    Setting up docusaurus for docs (#33)
    * Start docusaurus
    
    * Personalize
    
    * More groundlight
    
    * Try deploy
    
    * Change path
    
    * default dir
    
    * NPM
    
    * docs -> web
    
    * npm run build
    
    * web -> docs
    
    * GA
    
    * Try docs/build
    
    * Change baseUrl
    
    * Remove cache
    
    * Better action
    
    * Add cache
    
    * Test change
    
    * Add note
    
    * Theming improvements

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,142 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+// Options: https://github.com/FormidableLabs/prism-react-renderer/tree/master/src/themes
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Groundlight",
+  tagline: "Computer vision made simple",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://groundlight.ai",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/python-sdk/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "groundlight", // Usually your GitHub org/user name.
+  projectName: "python-sdk", // Usually your repo name.
+  deploymentBranch: "gh-pages", // Branch that GitHub pages will deploy from.
+  trailingSlash: false,
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: "img/docusaurus-social-card.jpg",
+      navbar: {
+        title: "Groundlight",
+        logo: {
+          alt: "Groundlight Logo",
+          src: "img/favicon-32x32.png",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Docs",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/groundlight/python-sdk",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Getting Started",
+                to: "/docs/getting-started",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Twitter",
+                href: "https://twitter.com/GroundlightAI",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/groundlight",
+              },
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/groundlight",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/groundlight/python-sdk",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Groundlight AI.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
