724cd3be5309650ca4234f0a19718bbf92da3acc
brandon <brandon@groundlight.ai>
2023-08-11 16:42:50 -0700

    Started writing this before realizing how wrongheaded the direction was

--- src/groundlight/cli.py
+++ src/groundlight/cli.py
@@ -3,24 +3,53 @@ Command line interface for groundlight
 """
 import fire
 from groundlight import Groundlight
+from typing import Optional
 
 
-def submit_image(detector_id: str, image_path: str, endpoint: str = "https://api.groundlight.ai/"):
+class GroundlightCLI:
     """
-    Submit an image for prediction
+    A series of command line tools to quickly interact with the Groundlight API
     """
-    gl = Groundlight(endpoint=endpoint)
-    print("Submitting image for prediction...")
-    result = gl.submit_image_query(detector_id, image_path)
-    print(result)
+
+    def list_detectors(self, page=1, page_size=10, endpoint: str = "https://api.groundlight.ai/"):
+        """
+        Gets a page of detectors out of all detectors
+        """
+        gl = Groundlight(endpoint=endpoint)
+        print("Getting detectors...")
+        result = gl.list_detectors(page=page, page_size=page_size)
+        print(result)
+
+    def get_detector(self, detector_id: str, endpoint: str = "https://api.groundlight.ai/"):
+        """
+        Get a detector by ID
+        Args:
+            detector_id: The ID of the detector to get, will match the string returned by
+        """
+        gl = Groundlight(endpoint=endpoint)
+        print("Getting detector...")
+        result = gl.get_detector_by_name(detector_id)
+        print(result)
+
+    def submit_image(
+        self,
+        detector_id: str,
+        image_path: str,
+        endpoint: str = "https://api.groundlight.ai/",
+    ):
+        """
+        Submit an image for prediction
+        Args:
+            detector_id: The ID of the detector to submit an image to, will match the string returned by
+        """
+        gl = Groundlight(endpoint=endpoint)
+        print("Submitting image for prediction...")
+        result = gl.submit_image_query(detector_id, image_path)
+        print(result)
 
 
 def main():
-    fire.Fire(
-        {
-            "submit_image": submit_image,
-        }
-    )
+    fire.Fire(GroundlightCLI)
 
 
 if __name__ == "__main__":
