f2f62cb0e59464a6e6e6b2f88db22e7e9a3f318f
blaise-muhirwa <blaise@groundlight.ai>
2023-10-09 16:52:41 -0700

    make method static to make pylint happy

--- src/groundlight/client.py
+++ src/groundlight/client.py
@@ -101,7 +101,8 @@ class Groundlight:
         self.detectors_api = DetectorsApi(self.api_client)
         self.image_queries_api = ImageQueriesApi(self.api_client)
 
-    def _fixup_image_query(self, iq: ImageQuery) -> ImageQuery:
+    @staticmethod
+    def _fixup_image_query(iq: ImageQuery) -> ImageQuery:
         """
         Process the wire-format image query to make it more usable.
         """
