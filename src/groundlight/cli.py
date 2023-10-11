04e4cdac266fde07dadaffca0b6c3d1e712598c6
brandon <brandon@groundlight.ai>
2023-10-11 00:00:53 -0700

    Pushes Groundlight class instantiation up until the function is actually called with arguments. This means that the entire help args are available even if we can't instantiate the class (no api key)

--- src/groundlight/cli.py
+++ src/groundlight/cli.py
@@ -18,9 +18,19 @@ def class_func_to_cli(method):
     Given the class method, simplify the typing on the inputs so that Typer can accept the method
     """
 
-    @wraps(method)
+    # We create a temporary class to bind the method to so we have the correct annotations for typer to use
+    # The first thing we'll do when we actually call the method is to bind it to a proper Groundlight instance
+    class fake_class:
+        pass
+
+    fake_instance = fake_class()
+    fake_bound_method = method.__get__(fake_instance, fake_class)
+
+    @wraps(fake_bound_method)
     def wrapper(*args, **kwargs):
-        print(method(*args, **kwargs))  # this is where we output to the console
+        gl = Groundlight()
+        gl_bound_method = fake_instance.fake_bound_method.__get__(gl, Groundlight)
+        print(gl_bound_method(*args, **kwargs))  # this is where we output to the console
 
     # not recommended practice to directly change annotations, but gets around Typer not supporting Union types
     for name, annotation in method.__annotations__.items():
@@ -35,12 +45,10 @@ def class_func_to_cli(method):
 
 def groundlight():
     try:
-        gl = Groundlight()
         # For each method in the Groundlight class, create a function that can be called from the command line
         for name, method in vars(Groundlight).items():
             if callable(method) and not name.startswith("_"):
-                attached_method = method.__get__(gl)
-                cli_func = class_func_to_cli(attached_method)
+                cli_func = class_func_to_cli(method)
                 cli_app.command()(cli_func)
         cli_app()
     except ApiTokenError:
