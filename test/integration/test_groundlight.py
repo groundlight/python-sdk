d60b3985e2a4ae5979a73dd9f91dff8dd6c91f30
blaise-muhirwa <blaise@groundlight.ai>
2023-10-11 16:46:06 -0700

    tested that docs are ready to deploy

--- test/integration/test_groundlight.py
+++ test/integration/test_groundlight.py
@@ -363,8 +363,7 @@ def test_submit_numpy_image(gl: Groundlight, detector: Detector):
     assert is_valid_display_result(_image_query.result)
 
 
-# @pytest.mark.skipif(MISSING_PIL, reason="Needs pillow")  # type: ignore
-@pytest.mark.skip
+@pytest.mark.skipif(MISSING_PIL, reason="Needs pillow")  # type: ignore
 def test_detector_improvement(gl: Groundlight):
     # test that we get confidence improvement after sending images in
     # Pass two of each type of image in
