8334d3d08b66fff5b87b62679cd45b42b63a23a8
brandon <brandon@groundlight.ai>
2024-03-12 14:11:19 -0700

    addressing comments before next big PR

--- src/groundlight/__init__.py
+++ src/groundlight/__init__.py
@@ -7,7 +7,7 @@ from model import *
 
 # Imports from our code
 from .client import Groundlight
-from .unstableapi import UnstableApi
+from .experimentalapi import ExperimentalApiApi
 from .binary_labels import Label
 from .version import get_version
 
