ccd972eb8d2619e18c7841cdc2364a011b2a9df1
Jesus Valle <jesus@leanware.io>
2024-03-12 20:19:48 -0500

    Add intercom chat

---
+++ docs/src/hooks/useIntercom.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+
+const useIntercom = () => {
+  useEffect(() => {
+    const loadIntercom = () => {
+      const w = window;
+      const ic = w.Intercom;
+      if (typeof ic === "function") {
+        ic("reattach_activator");
+        ic("update", w.intercomSettings);
+      } else {
+        const i = function () {
+          i.c(arguments);
+        };
+        i.q = [];
+        i.c = function (args) {
+          i.q.push(args);
+        };
+        w.Intercom = i;
+
+        const s = document.createElement("script");
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://widget.intercom.io/widget/fu9pkks5";
+        document.body.appendChild(s);
+      }
+    };
+
+    if (document.readyState === "complete") {
+      loadIntercom();
+    } else {
+      window.addEventListener("load", loadIntercom);
+    }
+
+    return () => window.removeEventListener("load", loadIntercom);
+  }, []);
+
+  useEffect(() => {
+    if (typeof window.Intercom === "function") {
+      window.Intercom("boot", {
+        api_base: "https://api-iam.intercom.io",
+        app_id: "fu9pkks5",
+        // name: `${user?.username}`,
+        // email: `${user?.email}`,
+      });
+    }
+
+    if (typeof window.Intercom === "function") {
+      window.Intercom("update", window.intercomSettings);
+    }
+  }, []);
+};
+
+export default useIntercom;
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -7,7 +7,7 @@ import clsx from "clsx";
 // import React from "react";
 // VSCode might try to delete it, but that will break the site.
 import React from "react";
-
+import useIntercom from "@site/src/hooks/useIntercom";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
@@ -32,6 +32,7 @@ function HomepageHeader() {
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
+  useIntercom();
   return (
     <Layout
       title={`${siteConfig.title}`}
