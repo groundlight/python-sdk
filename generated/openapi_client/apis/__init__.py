1416be94ca266a7de5ae39f733fd926e776a2153
Brandon <132288221+brandon-groundlight@users.noreply.github.com>
2024-03-12 17:12:14 -0700

    Notifications in SDK (#179)
    * wip. mix of whoami improvments and change to the public-api to support notifications
    
    * Experimenting with autogenerating the request function
    
    * Some debugging, but it seems like a valid request is sent. TODO, test with dev environment and make the sdk code cleaner and nicer
    
    * Automatically reformatting code
    
    * Seems to work. The parameters for each type of notification aren't documented yet as they may be unstable
    
    * Making very clear that this isn't in the final release of the SDK
    
    * Automatically reformatting code
    
    * make the UnstableApi class accessible like the Groundlight class
    
    * addressing comments before next big PR
    
    * typo
    
    * removing old file
    
    * lint
    
    * esoteric linter stuff
    
    * pylint is acting weird still
    
    * Ok, there was one legit linter error among several that I'm not too interested in
    
    * aaa
    
    * Automatically reformatting code
    
    ---------
    
    Co-authored-by: Auto-format Bot <autoformatbot@groundlight.ai>

--- generated/openapi_client/apis/__init__.py
+++ generated/openapi_client/apis/__init__.py
@@ -15,4 +15,5 @@
 # Import APIs into API package:
 from openapi_client.api.detectors_api import DetectorsApi
 from openapi_client.api.image_queries_api import ImageQueriesApi
+from openapi_client.api.rules_api import RulesApi
 from openapi_client.api.user_api import UserApi
