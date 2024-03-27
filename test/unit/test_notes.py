0b1144ba8b256454f779b6d8eec68ed2b50ab650
brandon <brandon@groundlight.ai>
2024-03-27 16:00:16 -0700

    customer types in test

--- test/unit/test_notes.py
+++ test/unit/test_notes.py
@@ -1,7 +1,8 @@
-from datetime import datetime, timezone
+from datetime import datetime
 
 import pytest
 from groundlight import ExperimentalApi
+from openapi_client.exceptions import ApiAttributeError
 
 
 @pytest.fixture(name="gl")
@@ -10,12 +11,21 @@ def _gl() -> ExperimentalApi:
 
 
 def test_notes(gl: ExperimentalApi):
-    name = f"Test {datetime.now(timezone.utc)}"
+    name = f"Test {datetime.utcnow()}"
     det = gl.create_detector(name, "test_query")
     gl.create_note(det, "test_note")
-    notes = gl.get_notes(det)["customer"]
-    found_note = False
-    for i in range(len(notes)):
-        if notes[i].content == "test_note":
-            found_note = True
-    assert found_note
+    # test runner could be either a customer or GL
+    try:
+        notes = gl.get_notes(det)["GL"]
+        found_note = False
+        for i in range(len(notes)):
+            if notes[i].content == "test_note":
+                found_note = True
+        assert found_note
+    except (AssertionError, ApiAttributeError) as e:
+        notes = gl.get_notes(det)["customer"]
+        found_note = False
+        for i in range(len(notes)):
+            if notes[i].content == "test_note":
+                found_note = True
+        assert found_note
