4e73c497b2223d0f6b1a53e6f8b753dfb8c7fa41
brandon <brandon@groundlight.ai>
2024-03-28 15:44:05 -0700

    Looks like the problem is that the different github runners all reached for the same detector at the same time

--- test/unit/test_actions.py
+++ test/unit/test_actions.py
@@ -1,10 +1,11 @@
 import pytest
 from groundlight import ExperimentalApi
 from openapi_client.exceptions import NotFoundException
-
+from datetime import datetime
 
 def test_create_action(gl: ExperimentalApi):
-    det = gl.get_or_create_detector("testing_detector", "test_query")
+    name = f"Test {datetime.utcnow()}"
+    det = gl.get_or_create_detector(name, "test_query")
     rule = gl.create_rule(det, "test_rule", "EMAIL", "test@example.com")
     rule2 = gl.get_rule(rule.id)
     assert rule == rule2
@@ -14,10 +15,11 @@ def test_create_action(gl: ExperimentalApi):
 
 
 def test_get_all_actions(gl: ExperimentalApi):
+    name = f"Test {datetime.utcnow()}"
     num_test_rules = 13  # needs to be larger than the default page size
     gl.ITEMS_PER_PAGE = 10
     assert gl.ITEMS_PER_PAGE < num_test_rules
-    det = gl.get_or_create_detector("test_detector", "test_query")
+    det = gl.get_or_create_detector(name, "test_query")
     gl.delete_all_rules()
     for i in range(num_test_rules):
         _ = gl.create_rule(det, f"test_rule_{i}", "EMAIL", "test@example.com")
--- test/unit/test_images.py
+++ test/unit/test_images.py
@@ -1,9 +1,11 @@
 import PIL
 from groundlight import ExperimentalApi
+from datetime import datetime
 
 
 def test_get_image(gl: ExperimentalApi):
-    det = gl.get_or_create_detector("test_detector", "test_query")
+    name = f"Test {datetime.utcnow()}"
+    det = gl.get_or_create_detector(name, "test_query")
     iq = gl.submit_image_query(det, image="test/assets/dog.jpeg", wait=10)
     gl.get_image(iq.id)
     assert isinstance(PIL.Image.open(gl.get_image(iq.id)), PIL.Image.Image)
