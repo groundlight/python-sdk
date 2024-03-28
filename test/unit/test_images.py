21366ca18e7c6580a926601af556a1d19953c97c
brandon <brandon@groundlight.ai>
2024-03-28 15:23:30 -0700

    running full tests with new tests completely disabled

--- test/unit/test_images.py
+++ test/unit/test_images.py
@@ -1,9 +1,9 @@
-import PIL
-from groundlight import ExperimentalApi
+# import PIL
+# from groundlight import ExperimentalApi
 
 
-def test_get_image(gl: ExperimentalApi):
-    det = gl.get_or_create_detector("test_detector", "test_query")
-    iq = gl.submit_image_query(det, image="test/assets/dog.jpeg", wait=10)
-    gl.get_image(iq.id)
-    assert isinstance(PIL.Image.open(gl.get_image(iq.id)), PIL.Image.Image)
+# def test_get_image(gl: ExperimentalApi):
+#     det = gl.get_or_create_detector("test_detector", "test_query")
+#     iq = gl.submit_image_query(det, image="test/assets/dog.jpeg", wait=10)
+#     gl.get_image(iq.id)
+#     assert isinstance(PIL.Image.open(gl.get_image(iq.id)), PIL.Image.Image)
--- test/unit/test_notes.py
+++ test/unit/test_notes.py
@@ -1,16 +1,16 @@
-from datetime import datetime
+# from datetime import datetime
 
-from groundlight import ExperimentalApi
+# from groundlight import ExperimentalApi
 
 
-def test_notes(gl: ExperimentalApi):
-    name = f"Test {datetime.utcnow()}"
-    det = gl.create_detector(name, "test_query")
-    gl.create_note(det, "test_note")
-    # test runner could be either a customer or GL
-    notes = (gl.get_notes(det).get("CUSTOMER") or []) + (gl.get_notes(det).get("gl") or [])
-    found_note = False
-    for i in range(len(notes)):
-        if notes[i].content == "test_note":
-            found_note = True
-    assert found_note
+# def test_notes(gl: ExperimentalApi):
+#     name = f"Test {datetime.utcnow()}"
+#     det = gl.create_detector(name, "test_query")
+#     gl.create_note(det, "test_note")
+#     # test runner could be either a customer or GL
+#     notes = (gl.get_notes(det).get("CUSTOMER") or []) + (gl.get_notes(det).get("gl") or [])
+#     found_note = False
+#     for i in range(len(notes)):
+#         if notes[i].content == "test_note":
+#             found_note = True
+#     assert found_note
