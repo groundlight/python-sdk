7a53a7327ee1b17c2caab090d6492914961f2914
brandon <brandon@groundlight.ai>
2024-03-28 18:28:53 -0700

    the line was too long...

--- test/unit/test_actions.py
+++ test/unit/test_actions.py
@@ -13,7 +13,7 @@ def test_create_action(gl: ExperimentalApi):
         gl.get_rule(rule.id)
 
 
-@pytest.mark.skip(reason="actions are global on an account, the test matrix will run multiple tests that collide")  # type: ignore
+@pytest.mark.skip(reason="actions are global on account, the test matrix collides with itself")  # type: ignore
 def test_get_all_actions(gl: ExperimentalApi):
     num_test_rules = 13  # needs to be larger than the default page size
     gl.ITEMS_PER_PAGE = 10
