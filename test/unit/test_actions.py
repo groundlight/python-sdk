ac1c11461e2fdfa30a2df1acdbf4a610fd2fa49d
Brandon <132288221+brandon-groundlight@users.noreply.github.com>
2024-04-03 15:27:05 -0700

    Unique detector name (#197)
    * Looks like the problem is that the different github runners all reached for the same detector at the same time
    
    * Make it easier to debug this next time by triggering the full tests manually through github
    
    * Automatically reformatting code
    
    ---------
    
    Co-authored-by: Auto-format Bot <autoformatbot@groundlight.ai>

--- test/unit/test_actions.py
+++ test/unit/test_actions.py
@@ -1,10 +1,13 @@
+from datetime import datetime
+
 import pytest
 from groundlight import ExperimentalApi
 from openapi_client.exceptions import NotFoundException
 
 
 def test_create_action(gl: ExperimentalApi):
-    det = gl.get_or_create_detector("testing_detector", "test_query")
+    name = f"Test {datetime.utcnow()}"
+    det = gl.get_or_create_detector(name, "test_query")
     rule = gl.create_rule(det, "test_rule", "EMAIL", "test@example.com")
     rule2 = gl.get_rule(rule.id)
     assert rule == rule2
@@ -15,10 +18,11 @@ def test_create_action(gl: ExperimentalApi):
 
 @pytest.mark.skip(reason="actions are global on account, the test matrix collides with itself")  # type: ignore
 def test_get_all_actions(gl: ExperimentalApi):
+    name = f"Test {datetime.utcnow()}"
     num_test_rules = 13  # needs to be larger than the default page size
     gl.ITEMS_PER_PAGE = 10
     assert gl.ITEMS_PER_PAGE < num_test_rules
-    det = gl.get_or_create_detector("test_detector", "test_query")
+    det = gl.get_or_create_detector(name, "test_query")
     gl.delete_all_rules()
     for i in range(num_test_rules):
         _ = gl.create_rule(det, f"test_rule_{i}", "EMAIL", "test@example.com")
