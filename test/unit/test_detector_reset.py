8ee319fa688d447f7d833b19f369fd99e27cfe76
brandon <brandon@groundlight.ai>
2024-08-28 15:11:49 -0700

    adding tests back in after bugfix

--- test/unit/test_detector_reset.py
+++ test/unit/test_detector_reset.py
@@ -6,17 +6,13 @@ from groundlight import ExperimentalApi
 from groundlight_openapi_client.exceptions import NotFoundException
 
 
-@pytest.mark.skip("Reset can potentially take a long time to get queued and run")
 def test_reset_retry(gl_experimental: ExperimentalApi):
     # Reset the detector, retrying in case the reset is still ongoing
-    print("Resetting detector")
     det = gl_experimental.create_detector(f"Test {datetime.utcnow()}", "test_query")
     iq = gl_experimental.submit_image_query(det, "test/assets/cat.jpeg")
     gl_experimental.reset_detector(det.id)
     success = False
-    for i in range(100):
-        time.sleep(60)
-        print(i)
+    for _ in range(60):
         try:
             gl_experimental.get_image_query(iq.id)
         except NotFoundException:
@@ -24,13 +20,11 @@ def test_reset_retry(gl_experimental: ExperimentalApi):
                 gl_experimental.get_image_query(iq.id)
             success = True
             break
+        time.sleep(10)
     if not success:
         raise Exception("Failed to reset detector")
-    else:
-        print("Successfully reset detector")
 
 
-@pytest.mark.skip("Reset can potentially take a long time to get queued and run")
 def test_reset_training(gl_experimental: ExperimentalApi):
     # If we reset a detector, we should have low confidence after the reset
     low_confidence_threshold = 0.6
