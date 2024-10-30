9820c188045feb84249aa90cceba50bb3629a02b
Tyler Romero <tyler@groundlight.ai>
2024-10-30 15:53:34 -0700

    Rename `app.groundlight.ai` to `dashboard.groundlight.ai` (#270)
    New login flow.

--- src/groundlight/config.py
+++ src/groundlight/config.py
@@ -1,4 +1,4 @@
-API_TOKEN_WEB_URL = "https://app.groundlight.ai/reef/my-account/api-tokens"
+API_TOKEN_WEB_URL = "https://dashboard.groundlight.ai/reef/my-account/api-tokens"
 API_TOKEN_VARIABLE_NAME = "GROUNDLIGHT_API_TOKEN"
 
 DEFAULT_ENDPOINT = "https://api.groundlight.ai/"
