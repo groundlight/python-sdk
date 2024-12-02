3284bbfdb050f5b236d7004b1022cf04c696ea03
Leo Dirac <leo@groundlight.ai>
2024-12-02 14:19:14 -0800

    Updating setup.py to minimum 3.9 python

--- generated/setup.py
+++ generated/setup.py
@@ -32,7 +32,7 @@ setup(
     author_email="support@groundlight.ai",
     url="",
     keywords=["OpenAPI", "OpenAPI-Generator", "Groundlight API"],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
