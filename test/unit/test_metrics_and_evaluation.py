3c566e6afd8edaa82740dc0044a1bdac297efd1f
brandon <brandon@groundlight.ai>
2025-02-21 17:22:35 -0800

    finalize test

--- test/unit/test_metrics_and_evaluation.py
+++ test/unit/test_metrics_and_evaluation.py
@@ -7,10 +7,24 @@ from groundlight import ExperimentalApi
 def test_metrics_and_evaluation(gl_experimental: ExperimentalApi):
     name = f"Test metrics and evaluation {datetime.utcnow()}"
     det = gl_experimental.create_detector(name, "test_query")
-    iq1 = gl_experimental.submit_image_query(det, "test/assets/cat.jpeg")
-    for i in range(10):
-        gl_experimental.add_label(iq1, "YES")
-        gl_experimental.add_label(iq1, "NO")
+    for i in range(6):
+        iq = gl_experimental.submit_image_query(det, "test/assets/cat.jpeg", wait=0, patience_time=10, human_review="NEVER")
+        gl_experimental.add_label(iq, "YES")
+        iq = gl_experimental.submit_image_query(det, "test/assets/cat.jpeg", wait=0, patience_time=10, human_review="NEVER")
+        gl_experimental.add_label(iq, "NO")
     metrics = gl_experimental.get_detector_metrics(det.id)
+    assert metrics["summary"] is not None
+    assert metrics["summary"]["num_ground_truth"] is not None
+    assert metrics["summary"]["num_current_source_human"] is not None
+    assert metrics["summary"]["class_counts"] is not None
+    assert metrics["summary"]["unconfident_counts"] is not None
+    assert metrics["summary"]["total_iqs"] is not None
 
-    evaluation = gl_experimental.evaluate_detector(det.id)
+    # NOTE: this implicitly tests how quickly the evaluation is made available
+    evaluation = gl_experimental.get_detector_evaluation(det.id)
+    assert evaluation["evaluation_results"]["kfold_pooled__balanced_accuracy"] is not None
+    assert evaluation["evaluation_results"]["kfold_pooled__positive_accuracy"] is not None
+    assert evaluation["evaluation_results"]["kfold_pooled__negative_accuracy"] is not None
+    assert evaluation["evaluation_results"]["balanced_system_accuracies"] is not None
+    assert evaluation["evaluation_results"]["positive_system_accuracies"] is not None
+    assert evaluation["evaluation_results"]["negative_system_accuracies"] is not None
