90651c5263bca31183f54aaac26bde1afc73c290
Francine Wright <fwright@hmc.edu>
2025-02-24 23:46:38 +0000

    test invalid payload templates

--- test/unit/test_actions.py
+++ test/unit/test_actions.py
@@ -184,3 +184,31 @@ def test_create_alert_webhook_action_headers(gl_experimental: ExperimentalApi):
     assert len(alert.webhook_action) == 1
     assert alert.webhook_action[0].payload_template.template == template
     assert alert.webhook_action[0].payload_template.headers == headers
+
+def test_create_alert_webhook_action_with_invalid_payload_template_headers(gl_experimental: ExperimentalApi):
+    name = f"Test {datetime.utcnow()}"
+    det = gl_experimental.get_or_create_detector(name, "test_query")
+    condition = gl_experimental.make_condition("CHANGED_TO", {"label": "YES"})
+    payload_template = gl_experimental.make_payload_template(
+        '{"template": "This is a fine template"}', headers="bad headers"
+    )
+    webhook_action = gl_experimental.make_webhook_action(
+        url="https://groundlight.ai", include_image=True, payload_template=payload_template
+    )
+
+    bad_request_exception_status_code = 400
+
+    with pytest.raises(ApiException) as e:
+        gl_experimental.create_alert(det, f"test_alert_{name}", condition, webhook_actions=webhook_action)
+    assert e.value.status == bad_request_exception_status_code
+
+    payload_template = gl_experimental.make_payload_template(
+        "This should not be a valid payload, it's valid jinja but won't produce valid json"
+    )
+    webhook_action = gl_experimental.make_webhook_action(
+        url="https://groundlight.ai", include_image=True, payload_template=payload_template
+    )
+
+    with pytest.raises(ApiException) as e:
+        gl_experimental.create_alert(det, f"test_alert_{name}", condition, webhook_actions=webhook_action)
+    assert e.value.status == bad_request_exception_status_code
