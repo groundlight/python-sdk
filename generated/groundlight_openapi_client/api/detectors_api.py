a467034318af513da30c0670643df7256fe8086e
brandon <brandon@groundlight.ai>
2025-05-27 21:42:27 -0700

    remove debug line

--- generated/groundlight_openapi_client/api/detectors_api.py
+++ generated/groundlight_openapi_client/api/detectors_api.py
@@ -156,7 +156,6 @@ class DetectorsApi:
         _body_params = None
         if _params["detector_creation_input_request"] is not None:
             _body_params = _params["detector_creation_input_request"]
-        import IPython; IPython.embed()
         # set the HTTP header `Accept`
         _header_params["Accept"] = self.api_client.select_header_accept(["application/json"])  # noqa: E501
 
